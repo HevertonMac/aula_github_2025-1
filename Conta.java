bb8332194bd12eba4084acf9221af3e882bf4a16
gutoportelaa <gutoportelaa@gmail.com>
2025-05-01 13:25:45 -0300

    Adicionando classes Conta e Cliente

---
+++ Conta.java
@@ -0,0 +1,35 @@
+public class Conta {
+    private String numero;
+    private double saldo;
+    private Cliente cliente;
+    
+    public Conta(String numero, double saldo, Cliente cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+    
+    public String getNumero() {
+        return numero;
+    }
+    
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public Cliente getCliente() {
+        return cliente;
+    }
+    
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
